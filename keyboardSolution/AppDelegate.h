b88565da71812e40a6f6c80fdb8e5cb91387e6df
john <john@johndeMacBook-Pro.local>
2014-12-04 21:23:48 +0800

    Initial Commit

---
+++ keyboardSolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  keyboardSolution
+//
+//  Created by john on 14-12-4.
+//  Copyright (c) 2014年 ___coco-sh___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ keyboardSolution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  keyboardSolution
+//
+//  Created by john on 14-12-4.
+//  Copyright (c) 2014年 ___coco-sh___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
